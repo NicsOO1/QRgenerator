8165affb11e2eb42b58002cac1e4dfc97b7bfacf
sbzz.in <124491086+NicsOO1@users.noreply.github.com>
2024-07-02 21:22:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const input = document.getElementById("inputfield");
+const qrImage = document.getElementById("qrImage");
+const qrBox = document.getElementById("qrBox");
+const btn = document.getElementById("btn")
+
+function generateQR() {
+  qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + input.value;
+
+  // Add a class to animate the QR code
+  qrImage.classList.add("animate-qr");
+
+  qrImage.style.display = "inline-block";
+}
+
+// Listen for the animation end event to remove the class
+qrImage.addEventListener("animationend", () => {
+  qrImage.classList.remove("animate-qr");
+});
